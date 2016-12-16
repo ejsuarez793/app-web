209940ec6b7e721cfb39843d9206238cf9e70226
ejsuarez793 <ejsuarez793@gmail.com>
2016-12-16 02:42:26 -0400

    Terminando de crear solicitud de presupuesto

--- app/controllers/proyectos/proyecto.js
+++ app/controllers/proyectos/proyecto.js
@@ -4,6 +4,23 @@ export default Ember.Controller.extend({
 	ecs: [],
 	sss: [],
 	mss: [],
+	presupuesto: {
+		codigo:'',
+		codigo_pro:'',
+		fecha: '',
+		fecha_m:'',
+		validez_o: '7',
+		descuento: '50',
+		observ: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.',
+		desc: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+		atencion_n: 'Ing Pedro Perez',
+		atencion_e: 'pperez@provincial.com',
+		materiales: [],
+		servicios:[],
+		cond_g:'Del fabricante.-',
+		cond_p: 'Precio sujeto a cambios sin previo aviso, dependerá de la alta rotación de inventario y disponibilidad.-\nEstos precios no incluyen transporte fuera del área metropolitana de Caracas.-\nEstos precios incluyen el Impuesto al Valor Agregado (IVA).-\n',
+		t_ent: 'Previa planificación con el cliente final.-',
+	},
 
 	init(){
 		this._super();
@@ -20,6 +37,9 @@ export default Ember.Controller.extend({
 
 		    url = window.serverUrl + '/servicio/';
 		    this.getElements(method,url,this.setServicios,this);
+
+		    this.set('presupuesto.fecha',moment().format("DD/MM/YYYY"));
+		    this.set('presupuesto.fecha_m',moment().format("LL"));
 		}
 	},
 	getElements(method,url,callback,context){
@@ -35,6 +55,25 @@ export default Ember.Controller.extend({
 		})    
 		.done(function(response){ callback(response, context); })    
 		.fail(function(response) { console.log(response); }); 
+	},
+	llamadaServidor(method,url,data){
+		$.ajax({
+			type: method,
+			url: url,
+			context: this,
+			headers:{
+				Authorization: "Token "+ Cookies.get('token'),
+			},
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				data: JSON.stringify(data),
+		})    
+		.done(function(response) { 
+			console.log(response);
+		})    
+		.fail(function(response) { 
+			console.log(response);
+		});
 	},
 	setProyecto(proyecto,context){
 		var _this = context;
@@ -69,7 +108,7 @@ export default Ember.Controller.extend({
 		});
 		if (cont < 26){
 			var np_cod = 'P'+arrayLetras[cont]+'-'+proyecto.codigo;
-			this.set('np_cod',np_cod);
+			this.set('presupuesto.codigo',np_cod);
 		}else{
 			console.log("El proyecto tiene mas de 26 presupuestos no se puede crear más");
 		}
@@ -123,7 +162,6 @@ export default Ember.Controller.extend({
 			$.each(elementos_cs.toArray(), function(i,elemento_cs){
 				if (elemento_cs.codigo === codigo){
 					if (elemento_cs.tag === 'servicio'){
-						console.log(elemento_cs);
 						servicios.pushObject(elemento_cs);
 					}else if(elemento_cs.tag === 'material'){
 						materiales.pushObject(elemento_cs);
@@ -134,8 +172,91 @@ export default Ember.Controller.extend({
 
 		});
 	},
-	actions: {
+	calcularPrecioTotal(codigo){
+		var cant = "#cantidad_"+codigo;
+		var pu = "#pu_"+codigo;
+		var pt = "#pt_"+codigo;
+		var ecs = this.get('ecs').toArray();
+		//console.log($(cant).val() + " * " + $(pu).val());
 
+		var precio_total =$(cant).val() * $(pu).val();
+		$(pt).text(precio_total);
+
+		$.each(ecs,function(i,elemento){
+			if (elemento.codigo ===codigo){
+				elemento.cant_s = $(cant).val();
+				elemento.precio_venta = $(pu).val();
+			}
+		});
+		//this.calcularMontoTotal();
+	},
+	calcularMontoTotal(){
+		var subtotal1 = parseFloat($("#subtotal1").text());
+		var descuento = parseFloat($("#descuento").text());
+		var subtotalfinal = parseFloat($("#subtotalfinal").text());
+		var iva = parseFloat($("#iva").text());
+		var total = parseFloat($("#total").text());
+		var cont = 0.0;
+		var pt = 0.0;
+
+		var elementos_cs = this.get('ecs');
+
+		$("td[name='precio_total']").each(function(i,item){
+			pt = parseFloat($(this).text());
+			cont += pt;
+		});
+
+		subtotal1 = cont;
+		descuento = cont *(parseFloat(this.get('presupuesto.descuento') / 100));
+		subtotalfinal =subtotal1 - descuento;
+		iva = subtotalfinal * (0.12);
+		total = subtotalfinal + iva;
+
+		$("#subtotal1").text(subtotal1);
+		$("#descuento").text(descuento);
+		$("#subtotalfinal").text(subtotalfinal);
+		$("#iva").text(iva);
+		$("#total").text(total);
+
+	},
+	prepararPresupuesto(){
+		var presupuesto = this.get('presupuesto');
+		var proyecto = this.get('proyecto');
+		var ecs = this.get('ecs').toArray();
+		var aux = {
+			codigo: '',
+			cantidad: '',
+			precio_venta: '',
+		}
+
+		presupuesto.codigo_pro = proyecto.codigo;
+		presupuesto.fecha = moment().format("YYYY-MM-DD");
+		presupuesto.servicios = [];
+		presupuesto.materiales = [];
+		$.each(ecs,function(i,elemento){
+			aux.codigo = elemento.codigo;
+			aux.cantidad = elemento.cant_s;
+			aux.precio_venta = elemento.precio_venta;
+			if (elemento.tag === 'servicio'){
+				presupuesto.servicios.push($.extend(true, {}, aux));
+			}else if(elemento.tag === 'material'){
+				presupuesto.materiales.push($.extend(true, {}, aux));
+			}
+		});
+
+		this.set('presupuesto',presupuesto);
+		console.log(proyecto);
+	},
+	actions: {
+		procesar:function(){
+			console.log("procesando..");
+			this.prepararPresupuesto();
+
+			var method = "POST";
+			var url = window.serverUrl + '/proyecto/' + this.get('proyecto.codigo')+'/presupuesto/';
+			var data = this.get('presupuesto');
+			this.llamadaServidor(method,url,data);
+		},
 		openModalPresupuesto: function(){
 			this.prepararModalPresupuesto();
 			$("#myModalPresupuesto").modal('show');
@@ -178,6 +299,9 @@ export default Ember.Controller.extend({
 			$.each(aux, function(i,elemento){
 				if ($.inArray(elemento.codigo, cod_cs) !== -1){
 					elemento.tag = tag;
+					if (tag ==='servicio'){
+						elemento.cantidad = 0;
+					}
 					elementos_cs.pushObject(elemento);
 				}
 			});
@@ -194,6 +318,12 @@ export default Ember.Controller.extend({
 		eliminarElementos: function(){
 			//console.log("eliminar");
 			this.eliminarElementos();
-		}
+		},
+		calcularPrecioTotal: function(codigo){
+			this.calcularPrecioTotal(codigo);
+		},
+		calcularMontoTotal: function(){
+			this.calcularMontoTotal();
+		},
 	}
 });
